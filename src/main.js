3dbc24415dbef4e1eb56186320f660d82e55b037
Kevin Bigelow <kbigelow5@gmail.com>
2019-10-11 21:03:24 -0600

    uninstalling firebase and vuefire. installing axios

--- src/main.js
+++ src/main.js
@@ -1,19 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { firestorePlugin } from 'vuefire'
-
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-
-Vue.use(firestorePlugin)
-
-firebase.initializeApp({
-  projectId: 'portfolio-kb',
-  databaseURL: 'https://portfolio-kb.firebaseio.com'
-})
-
-export const db = firebase.firestore()
 
 Vue.config.productionTip = false
 
