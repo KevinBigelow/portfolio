35854e96b580629d04162fa39b354422f10e0a86
Kevin Bigelow <kbigelow5@gmail.com>
2019-10-22 07:48:09 -0600

    specimen tile used on home page

---
+++ src/components/SpecimenTile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="column is-half">
+    <router-link :to="{name: 'specimen', params: {slug: specimen.slug}}">
+      <img :src="require(`@/assets/${specimen.slug}/${specimen.featured_image.src}`)">
+      {{ specimen.name }}
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    specimen: {}
+  }
+}
+</script>
