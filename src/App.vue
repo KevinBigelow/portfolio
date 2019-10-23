998af9b36c60190d11fefba20d57addc6fc9db94
Kevin Bigelow <kbigelow5@gmail.com>
2019-10-23 00:21:35 -0600

    some styles and layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app" class="app-layout">
-    <nav class="navbar-side">
-      <h2 class="has-text-primary is-size-3 has-text-centered">Designer & Developer</h2>
-      <h1 class="title is-size-1 has-text-centered">Kevin Bigelow</h1>
+    <nav class="app-navbar">
+      <div class="align-self--start">
+        <h2 class="has-text-primary is-size-3 has-text-centered">UI Designer & Developer</h2>
+        <h1 class="title is-size-1 has-text-centered">Kevin Bigelow</h1>
+      </div>
+      <div class="align-self--end">
+        <span>kevinbigelow.design@gmail.com</span>
+      </div>
     </nav>
     <article class="app-content">
       <router-view/>
