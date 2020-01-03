6a3a9d99d06cc776f3e33c290b000305ebc614f9
Kevin Bigelow <kbigelow5@gmail.com>
2020-01-03 00:28:56 -0700

    roughed out the Watchlist page, added some spacing utility classes

--- src/views/ProtonWatchlists.vue
+++ src/views/ProtonWatchlists.vue
@@ -3,7 +3,55 @@
     <article v-if="specimen">
       <SpecimenHeading :specimen="specimen"></SpecimenHeading>
       <div class="well well-vertical-gradient mt-3">
-        <img :src="getImgSrc(specimen.slug, specimen.featured_image.src)" class="is-full-width">
+        <ImageComponent :directory="specimen.slug" :image="specimen.images['dashboard']" classes="is-full-width mb-5"></ImageComponent>
+      </div>
+      <ul>
+        <li class="flex blurb-with-image">
+          <div>
+            <h3 class="has-text-primary is-size-3">Adding pages to watchlists</h3>
+            <p class="is-family-sans-serif mb-1">Users had to save pages at various depths within Proton.</p>
+            <p class="is-family-sans-serif mb-1">Adding this button to each available page met the user where they were.</p>
+            <p class="is-family-sans-serif mb-1">This button is a bookmark with superpowers.</p>
+          </div>
+          <div class="pt-4 ph-1">
+            <ImageComponent :directory="specimen.slug" :image="specimen.images['addToWatchlistButton']" classes=""></ImageComponent>
+          </div>
+        </li>
+        <li class="flex blurb-with-image">
+          <div>
+            <h3 class="has-text-primary is-size-3">The first interaction</h3>
+            <p class="is-family-sans-serif mb-1">No watchlists would exist for the user at this point. This is the flow for that use case.</p>
+            <p class="is-family-sans-serif mb-1">When the dialog opens, the field is active and ready for input.</p>
+            <p class="is-family-sans-serif mb-1">Once the user types anything into the input field, the save button appears.</p>
+          </div>
+          <div class="pt-2 ph-1">
+            <ImageComponent :directory="specimen.slug" :image="specimen.images['createWatchlist']" classes=""></ImageComponent>
+          </div>
+        </li>
+        <li class="flex blurb-with-image">
+          <div>
+            <h3 class="has-text-primary is-size-3">If watchlists exist</h3>
+            <p class="is-family-sans-serif mb-1">Any watchlist created by the user is displayed and easily added to with the click of a button.</p>
+          </div>
+          <div class="pt-2 ph-1">
+            <ImageComponent :directory="specimen.slug" :image="specimen.images['addToWatchlist']" classes=""></ImageComponent>
+          </div>
+        </li>
+        <li class="flex blurb-with-image">
+          <div>
+            <h3 class="has-text-primary is-size-3">What if the page has been added already?</h3>
+            <p class="is-family-sans-serif mb-1">Any watchlist that already includes the page will be listed, but disabled and grayed out.</p>
+            <p class="is-family-sans-serif mb-1">On hover, a message is displayed.</p>
+          </div>
+          <div class="pt-2 ph-1">
+            <ImageComponent :directory="specimen.slug" :image="specimen.images['existsInWatchlist']" classes=""></ImageComponent>
+          </div>
+        </li>
+      </ul>
+      <div class="well well-vertical-gradient mt-3">
+        <a href="#">
+          <ImageComponent :directory="specimen.slug" :image="specimen.images['prototypeLink']" classes="is-full-width"></ImageComponent>
+        </a>
       </div>
     </article>
   </div>
@@ -11,6 +59,7 @@
 
 <script>
   import SpecimenHeading from "../components/SpecimenHeading";
+  import ImageComponent from "../components/ImageComponent";
   import fetchSpecimen from '../mixins/fetchSpecimen.js';
   import getImgSrc from "../mixins/getImgSrc";
 
@@ -19,7 +68,22 @@
     props: ['slug'],
     mixins: [fetchSpecimen, getImgSrc],
     components: {
-      SpecimenHeading
+      SpecimenHeading, ImageComponent
     },
   }
 </script>
+
+<style scoped>
+  .blurb-with-image {
+    flex-direction: column;
+    margin-bottom: 10rem;
+  }
+  @media screen and (min-width: 1000px) {
+    .blurb-with-image {
+      flex-direction: row;
+    }
+    .blurb-with-image > * {
+      width: 50%;
+    }
+  }
+</style>
