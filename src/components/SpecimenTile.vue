f4b0aa20fdad888d1c673eb882e5a5e31dae0cef
Kevin Bigelow <kbigelow5@gmail.com>
2020-01-21 23:44:25 -0700

    scoping specimen tile styles to single file component

--- src/components/SpecimenTile.vue
+++ src/components/SpecimenTile.vue
@@ -14,3 +14,44 @@
     }
   }
 </script>
+
+<style scoped lang="scss">
+  .specimen-tile {
+      min-height: 400px;
+      margin-bottom: 2rem;
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      position: relative;
+      transition: border-color .25s;
+      border-radius: $border-radius-5;
+      overflow: hidden;
+
+      .specimen-link-text, &:after {
+          transition: opacity .25s;
+      }
+
+      &:after {
+          content: '';
+          background: $primary;
+          position: absolute;
+          display: block;
+          top: 0;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          z-index: 0;
+          opacity: .85;
+      }
+
+      .specimen-link-text {
+          z-index: 1;
+      }
+
+      &:not(:hover) {
+          .specimen-link-text, &:after {
+              opacity: 0;
+          }
+      }
+  }
+</style>
