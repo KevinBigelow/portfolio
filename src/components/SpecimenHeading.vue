1eedfd364d1a9ac31d980fb2e5d6645e88426126
Kevin Bigelow <kbigelow5@gmail.com>
2020-02-01 14:18:37 -0700

    conditional commas

--- src/components/SpecimenHeading.vue
+++ src/components/SpecimenHeading.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="specimen">
     <h1 class="title is-size-2">{{specimen.name}}</h1>
-    <h2 class="subtitle is-size-4 is-size-3-fullhd has-text-primary mb-2"><span v-for="cat in specimen.categories" :key="cat">{{cat}}, </span></h2>
+    <h2 class="subtitle is-size-4 is-size-3-fullhd has-text-primary mb-2"><span v-for="(cat, index) in specimen.categories" :key="cat">{{cat}}<span v-if="index !== specimen.categories.length - 1">, </span></span></h2>
   </div>
 </template>
 
