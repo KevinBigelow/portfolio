e8e5349f2f029ab4f56f0bf6aa1cf3cf9eb3945c
Kevin Bigelow <kbigelow5@gmail.com>
2020-02-25 01:02:36 -0700

    adjusting .specimen-tile-meta transition

--- src/components/SpecimenTile.vue
+++ src/components/SpecimenTile.vue
@@ -1,6 +1,6 @@
 <template>
   <KinesisContainer event="mouse" class="is-relative specimen-tile-container">
-    <div class="is-absolute specimen-tile-meta-wrap flex--column justify-content--end">
+    <router-link :to="{name: specimen.slug, params: {slug: specimen.slug}}" class="is-absolute specimen-tile-meta-wrap flex--column justify-content--end">
       <div class="specimen-tile-meta card border-radius--5">
         <div class="card-header flex--row justify-content--space-between">
           <div class="pv-2 ph-2">
@@ -13,13 +13,12 @@
           <span class="is-size-4 specimen-tile-meta-icon card-header-icon">&rsaquo;</span>
         </div>
       </div>
-    </div>
+    </router-link>
 
     <KinesisElement type="depth" :strength="4" class="mb-3">
-      <router-link :to="{name: specimen.slug, params: {slug: specimen.slug}}"
-                   class="specimen-tile flex align-items--end justify-content--center box-shadow--gray-light"
+      <div class="specimen-tile flex align-items--end justify-content--center box-shadow--gray-light"
                    :style="{'background-image': 'url(' + require(`@/assets/${specimen.slug}/${specimen.featured_image.src}`) + ')'}">
-      </router-link>
+      </div>
       <KinesisElement :strength="18" class="specimen-tile-shadow"></KinesisElement>
     </KinesisElement>
   </KinesisContainer>
