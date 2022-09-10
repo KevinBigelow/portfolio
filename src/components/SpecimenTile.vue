dddf25524bc647a2c20f36fd083e30f5bfaa3e0a
Kevin Bigelow <kbigelow5@gmail.com>
2022-09-10 16:43:08 -0600

    some stylistic updates

--- src/components/SpecimenTile.vue
+++ src/components/SpecimenTile.vue
@@ -2,16 +2,15 @@
 <!--  <KinesisContainer event="mouse" class="is-relative specimen-tile-container">-->
   <div class="is-relative specimen-tile-container">
     <router-link :to="{name: specimen.slug, params: {slug: specimen.slug}}" class="is-absolute specimen-tile-meta-wrap flex--column justify-content--end">
-      <div class="specimen-tile-meta card border-radius--5">
-        <div class="card-header flex--row justify-content--space-between">
+      <div class="specimen-tile-meta border-radius--5">
+        <div class="flex--row justify-content--space-between">
           <div class="pv-2 ph-2">
-            <h4 class="is-size-5 has-text-weight-bold specimen-tile-meta-title">{{specimen.name}}</h4>
+            <h4 class="title is-size-3 has-text-weight-bold specimen-tile-meta-title">{{specimen.name}}</h4>
             <ul class="subtitle flex--row flex-wrap">
-              <li v-for="(cat, index) in specimen.categories" :key="cat" class="is-size-5 specimen-tile-meta-category">
+              <li v-for="(cat, index) in specimen.categories" :key="cat" class="is-size-4 specimen-tile-meta-category">
                 {{cat}}<span v-if="index !== specimen.categories.length - 1">,&#32;</span></li>
             </ul>
           </div>
-          <span class="is-size-4 specimen-tile-meta-icon card-header-icon">&rsaquo;</span>
         </div>
       </div>
     </router-link>
