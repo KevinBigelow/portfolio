c65780c7dc0a6e0e81570e974705be9f7a632688
Kevin Bigelow <kbigelow5@gmail.com>
2022-09-21 00:49:47 -0600

    updating resume

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -4,7 +4,7 @@
         <h1 class="app-navbar--title title has-text-white grid-name is-marginless"><router-link :to="{name: 'home'}" class="has-text-white">Kevin Bigelow</router-link></h1>
         <div class="grid-actions">
             <KinesisElement type="depth" :strength="9">
-                <a :href="baseUrl + 'kevinbigelow_resume_8-18-22.pdf'"
+                <a :href="baseUrl + 'kevinbigelow_resume.pdf'"
                    target="_blank"
                    class="app-navbar--button button is-layered is-primary-gradient is-primary is-medium is-margin-centered is-block"><font-awesome-icon class="mr-1" :icon="['far', 'file-pdf']" /> Résumé</a>
             </KinesisElement>
