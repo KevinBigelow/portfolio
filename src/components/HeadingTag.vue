0f8ddc20e25486ad7f37939da45486b9371d4853
Kevin Bigelow <kbigelow5@gmail.com>
2022-09-29 00:44:05 -0600

    some typography components

---
+++ src/components/HeadingTag.vue
@@ -0,0 +1,24 @@
+<template>
+    <component :is="headingTag" class="mb-2 mt-2" :class="`is-size-${level}`">
+        <slot/>
+    </component>
+</template>
+
+<script>
+    export default {
+        props: {
+            level: {
+                type: Number,
+                required: true,
+                validate: (value) => {
+                    return [1,2,3,4,5,6].includes(value)
+                }
+            }
+        },
+        computed: {
+            headingTag() {
+                return 'h' + this.level
+            }
+        }
+    }
+</script>
